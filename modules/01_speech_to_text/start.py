0844a1075d9180c47e159f9be4ac1daeb3427c70
gregghy <gregoriosilves@gmail.com>
2022-09-12 15:10:49 +0200

    Initial commit

--- modules/01_speech_to_text/start.py
+++ modules/01_speech_to_text/start.py
@@ -1,6 +1,6 @@
 # Replace the None values in the following variables according to README.md
-speech_key = None
-language_key = None
+speech_key = '0bd1085c61894457ae7eeced74f1ae9f'
+language_key = '10059fe01a184784b00605bcc8db031e'
 language_region = 'westeurope'
-language_endpoint = None
+language_endpoint = 'https://sjf22-language-gregghy.cognitiveservices.azure.com/'
 language_project = 'sjf22-clu'
